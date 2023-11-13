171d2b3db2bcd125602d49625b4327b141b3fd01
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-13 15:31:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function submitForm() {
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
+      const phoneNumber = document.getElementById('phoneNumber').value;
+      const email = document.getElementById('email').value;
+
+      const formInfo = `First Name: ${firstName}\nLast Name: ${lastName}\nPhone Number: ${phoneNumber}\nEmail ID: ${email}`;
+
+      alert(formInfo);
+    }
